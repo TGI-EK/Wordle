5bf25d009dd30f465e3f9402023ec19ac595ba9d
Ichmagmathe <>
2022-05-11 18:08:41 +0200

    reworked WordListe

--- src/main/java/wordlegruppe/wordle/game/WordListe.java
+++ src/main/java/wordlegruppe/wordle/game/WordListe.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Clock;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,12 +13,12 @@ import java.time.Clock;
 public class WordListe {
 
     // Name und Pfad der Datei 
-    private String fileName = "src\\main\\java\\wordlegruppe\\wordle\\game\\en_dict.txt";
+    private String fileName = "src\\main\\resources\\textfile\\en_dict.txt";
 
-    // Erstellt ein Array fuer die Woerter
-    private String[] liste = new String[count_lines()];
+    // Erstellt ein ArrayList fuer die Woerter
+    private ArrayList<String> liste = new ArrayList<>();
 
-    public String[] open_file() {
+    public ArrayList<String> openFile() {
 
         try {
             // FileReader liest die Text-Datei, default: encoding.
@@ -30,7 +30,7 @@ public class WordListe {
             int i = 0;
 
             while ((line = bufferedReader.readLine()) != null) {
-                liste[i] = line;
+                liste.add(line);
                 i++;
             }
 
@@ -46,45 +46,30 @@ public class WordListe {
                     + fileName + "'");
         }
         
-        return liste;      
+        return liste;
     }
     
-    private int count_lines() {
-
-        int lines = 0;
-
-        try ( BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            while (reader.readLine() != null) {
-                lines++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return lines;
-    }
-
-    public String random_word() {
+    public String randomWord() {
         String random_word;
-        open_file();
+        if(liste.isEmpty())openFile();    
         //Gib eine Zahl zwischen 0 und der Maximalen Anzahl an Zeilen
-        int random_number = (int) Math.round(Math.random() * count_lines());
+        int random_number = (int) Math.round(Math.random() * liste.size());
 
         //Waehlt ein Zufaelliges Wort 
-        random_word = liste[random_number];
+        random_word = liste.get(random_number);
 
         return random_word;
     }
     
-    public boolean check_for_word(String inputWord)
+    public boolean checkForWord(String inputWord)
     {
-        open_file();
+        if(liste.isEmpty())openFile();
         boolean inList = false;
         
-        for(int i = 0; i < count_lines(); i++)
+        for(int i = 0; i < liste.size(); i++)
         {
             // Check ob das Wort in der List ist; Beide werden klein gemacht um sie besser zu vergleichen
-            if(liste[i].toLowerCase().equals(inputWord.toLowerCase())) inList = true;                     
+            if(liste.get(i).toLowerCase().equals(inputWord.toLowerCase())) inList = true;                     
         }
         
         return inList;
